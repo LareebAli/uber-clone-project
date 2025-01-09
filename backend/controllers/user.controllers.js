c1c5592f4195ea931d3b25496264cae42de980cf
LareebAli <laraibalilaraib02@gmail.com>
2025-01-09 22:19:05 +0500

    Login Api

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,24 +1,52 @@
-const userModel = require('../models/user.model')
-const userService =require ("../services/user.service")
-const {validationResult}= require('express-validator')
+const userModel = require('../models/user.model');
+const userService = require("../services/user.service");
+const { validationResult } = require('express-validator');
 
-module.exports.registerUser = async (req,res,next)=>{
-const error =validationResult(req)
-if(!error.isEmpty()){
-    return res.status(400).json({errors: error.array()})
-}
+module.exports.registerUser = async (req, res, next) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res.status(400).json({ errors: error.array() });
+    }
 
+    const { fullname, email, password } = req.body;
+    const hashPassword = await userModel.hashPassword(password);
+    
+    // User creation using userService
+    const user = await userService.createUser({
+        firstname: fullname.firstname,
+        lastname: fullname.lastname,
+        email,
+        password: hashPassword
+    });
 
-const {fullname,email,password}=req.body
-const hashPassword = await userModel.hashPassword(password)
-const  user = await userService.createUser({
-    firstname:fullname.firstname,
-    lastname:fullname.lastname,
-    email,
-    password:hashPassword
-});
-const token= user.generateAuthToken()
-res.status(201).json({token, user})
+    const token = user.generateAuthToken();
+    res.status(201).json({ token, user });
+};
 
+module.exports.loginUser = async (req, res, next) => {
 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = user.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, user });
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const {body}=require('express-validator')
-const userController = require('../controllers/user.controllers')
+const { body } = require('express-validator');
+const userController = require('../controllers/user.controllers');
 
-router.post('/register',[
-    body('email').isEmail().withMessage('invalid email'),
-    body('fullname.firstname').isLength({min:3}).withMessage('First name must be at least 3 Character long'),
-    body('password').isLength({min:6}).withMessage("password must bet at least 6 Character long ")
-],userController.registerUser)
+// Register Route
+router.post('/register', [
+    body('email')
+        .isEmail()
+        .withMessage('Invalid email'),
+    
+    body('fullname.firstname')
+        .not()
+        .isEmpty()
+        .withMessage('First name is required')
+        .isLength({ min: 3 })
+        .withMessage('First name must be at least 3 characters long'),
 
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage("Password must be at least 6 characters long")
+], userController.registerUser);
 
-
-
-
-
-
-
-
-
-module.exports = router
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    userController.loginUser
+)
+module.exports = router;
